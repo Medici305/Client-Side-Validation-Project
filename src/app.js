ce0733ce2b1a282352fa7ff19e409205d00e5e68
medici305 <asvpescobar@hotmail.com>
2020-11-05 15:21:59 +0000

    modal

---
+++ src/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.modal').modal();
+});
+
+const trial = document.querySelector('.free-trial');
+trial.addEventListener('click', e => {
+    //e.target.style.backgroundColor = 'blue';
+})
